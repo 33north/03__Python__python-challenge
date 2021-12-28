2f1b5975d22e497416a485695fd566c562a05869
admin <rmbp2015@rMBP2015-admin.local>
2021-12-28 02:45:45 -0800

    20211228 - Created directories and files for python-challenge
    - Created PyBank and PyPoll directories.
      - Each directories comes with resources and analysis directory, and also each comes with a main.py file.
    - Worked mainly on the main.py for PyBank.  Only started a small bit of the main.py for PyPoll.

---
+++ PyPoll/main.py
@@ -0,0 +1,1 @@
+# Importing modules/packagesimport csvimport osimport pprint as pp# Define the csvpath variable using os.path.join methodcsvpath = os.path.join("resources", "election_data.csv")
